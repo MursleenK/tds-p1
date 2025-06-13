0ba1ac8b7907dc33b0b93f66ade7d9d6ad5073bf
MurK <84435568+MursleenK@users.noreply.github.com>
2025-06-14 02:35:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
